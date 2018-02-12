5ea566f31e1dcc40045c3668f0516f642d217d1d
Alex <dacljc@gmail.com>
2018-02-12 18:03:20 +1100

    changed independency to boolean

--- throwing_a_dice/ThrowingADice/Program.cs
+++ throwing_a_dice/ThrowingADice/Program.cs
@@ -7,9 +7,9 @@ namespace ThrowingADice
     {
         static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int k = Program.AddNumbers(3, 4);
-            Console.WriteLine(k);
+            //Console.WriteLine("Hello World!");
+            //int k = Program.AddNumbers(3, 4);
+            //Console.WriteLine(k);
             int[] p = new int[] {1, 2, 3, 4, 5 ,6};
             int[] a = new int[] {1, 2};
             int[] b = new int[] {2, 1};
@@ -25,18 +25,20 @@ namespace ThrowingADice
 
         public static void IndependencyCheck(int[] p, int[] a, int[] b)
         {
+            /*
             for (int i = 0; i < p.Length; i++)
             {
                 Console.WriteLine(p[i]);
-            }
+            }            
+            */
+
             int numberOfElementsInA = a.Length;
             int numberOfElementsInB = b.Length;
-
             var intersection = new List<int>();
 
-            for (int i = 0; i < a.Length; i++)
+            for (int i = 0; i < numberOfElementsInA; i++)
             {
-                for (int j = 0; j < b.Length; j++)
+                for (int j = 0; j < numberOfElementsInB; j++)
                 {
                     if (a[i] == b[j])
                     {
@@ -47,8 +49,10 @@ namespace ThrowingADice
             }
             if (intersection.Count == 0)
             {
-                string message = "Events a and b are independent.";
-                Console.WriteLine(message);
+                //string message = "Events a and b are independent.";
+                //Console.WriteLine(message);
+                bool independency = true;
+                Console.WriteLine(independency);
             }
             else 
             {
@@ -56,13 +60,17 @@ namespace ThrowingADice
                 float abProduct = a.Length * b.Length / (p.Length) ^ 2;
                 if (abIntersection == abProduct)
                 {
-                    string message = "Events a and b are dependent.";
-                    Console.WriteLine(message);
+                    //string message = "Events a and b are independent.";
+                    //Console.WriteLine(message);
+                    bool independency = true;
+                    Console.WriteLine(independency);
                 }
                 else
                 {
-                    string message = "Events a and b are independent.";
-                    Console.WriteLine(message);
+                    //string message = "Events a and b are dependent.";
+                    //Console.WriteLine(message);
+                    bool independency = false;
+                    Console.WriteLine(independency);
                 }
             }
         }
